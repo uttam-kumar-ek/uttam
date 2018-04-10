bb1f3dd01f97385b4c5b2b6612c522d15521473a
uttam-kumar-ek <uttam.kumar.c@netradyne.com>
2018-04-10 11:09:18 +0530

    uttam-kumar-ek:201820180410:Dictionary concept in python

---
+++ dictionary.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+dict = {}        #Dictionaries are inclosed by curly braces
+dict['one'] = "This is one"
+dict[2] = "This is two"
+tinydict = {'name': 'uttam','code':6734, 'dept': 'developement'}
+print dict['one'] # Prints value for 'one' key
+print dict[2] # Prints value for 2 key
+print tinydict # Prints complete dictionary
+print tinydict.keys() # Prints all the keys
+print tinydict.values() # Prints all the values
